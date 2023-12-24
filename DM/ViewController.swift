299473e04d5180595f2d32231e71026ef46509f5
Ibrohim Begaliev <81876891+ibegaliev@users.noreply.github.com>
2023-12-24 14:50:18 +0500

    initial commit

---
+++ DM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DM
+//
+//  Created by iBegaliev on 12/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
